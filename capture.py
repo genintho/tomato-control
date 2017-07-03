69036e9e3924bb2db1487424ab70b79d9257b67e
Thomas Genin <xt6.thomas.genin@gmail.com>
2017-07-02 18:59:13 -0700

    rotate image

--- capture.py
+++ capture.py
@@ -10,7 +10,7 @@ ROOT_DIR = "capture/"
 print "Start loop"
 while 1:
     today = time.strftime("%Y_%m_%d")
-    now = time.strftime("%H:%M:%S")
+    now = time.strftime("%H_%M_%S")
     active_dir = path.join(ROOT_DIR, today)
 
     if not os.path.exists(active_dir) or not path.isdir(active_dir):
@@ -22,17 +22,13 @@ while 1:
     image_path = path.join(active_dir, "{}-{}.jpg".format(today, now))
     tmp_image_path = path.join(active_dir, "tmp_{}-{}.jpg".format(today, now))
     print "Capture  image {}".format(image_path)
+    # TODO param
+    camera.rotation = 180
     camera.capture(image_path)
     camera.close()  # Destroy camera to save power
     print "Optimize image {}".format(image_path)
     system("convert -strip -quality 85% {} {}".format(image_path, tmp_image_path))
     os.remove(image_path)
     os.rename(tmp_image_path, image_path)
+    # TODO param
     sleep(2)
-
-
-print "Start convert"
-system('convert -delay 10 -loop 0 image*.jpg animation.gif')
-
-
-
