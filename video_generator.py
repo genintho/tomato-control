4d660dcbefd5f78162d94ed4379a86e85ad1cd45
Thomas Genin <xt6.thomas.genin@gmail.com>
2017-07-03 17:43:38 -0700

    use double quote every where

--- video_generator.py
+++ video_generator.py
@@ -8,6 +8,8 @@ from lib.youtube import get_most_recent_video_name, upload_video, insert_into_pl
 
 DATE_FORMAT = "%Y_%m_%d"
 TMP_CAPTURE_DIR = "/tmp/tomato-control-pi-capture"
+
+# TODO param
 PLAYLIST_ID = "PL4BgsQyEYodMayOiajRXmDkQ1NsWKccPP"
 
 # # 1. Get the date of the most recent upload on Youtube
@@ -18,7 +20,7 @@ if m is None:
     exit(1)
 
 yt_date = datetime.strptime(m.group(), DATE_FORMAT)
-# yt_date = datetime.strptime('2017-06-02', ISO_DATE_FORMAT)
+# yt_date = datetime.strptime("2017-06-02", ISO_DATE_FORMAT)
 print "YouTube date {}".format(yt_date)
 
 # 2. rsync with the pi the list of folder / files
@@ -27,6 +29,7 @@ if not os.path.exists(TMP_CAPTURE_DIR) or not path.isdir(TMP_CAPTURE_DIR):
     os.makedirs(TMP_CAPTURE_DIR)
 
 print "Rsync start ..."
+# TODO param
 system("rsync pi@10.0.1.12:tomato-control/capture/ {} -r --ignore-existing --stats".format(TMP_CAPTURE_DIR))
 print "Rsync done!"
 
@@ -34,7 +37,7 @@ print "Rsync done!"
 # 3. Get the most recent image folder coming from rsync
 tmp_content = os.listdir(TMP_CAPTURE_DIR)
 # filter out "." directories
-tmp_content = [x for x in tmp_content if x[:1] != '.']
+tmp_content = [x for x in tmp_content if x[:1] != "."]
 tmp_content.sort()
 
 active_pi_capture = datetime.strptime(tmp_content[-1], DATE_FORMAT)
@@ -71,7 +74,7 @@ for inc in range(0, dayDelta-1):
     for counter, img in enumerate(dir_content):
         src = path.join(target_dir, img)
         dest = path.join(tmpdirname, "img-{}.jpg".format(counter+1))
-        print 'file {} -> {}'.format(src, dest)
+        print "file {} -> {}".format(src, dest)
         shutil.copyfile(src, dest)
 
     # Run ffmpeg
@@ -85,6 +88,6 @@ for inc in range(0, dayDelta-1):
 
     # 7. remove the tmp files
     shutil.rmtree(tmpdirname)
-    print "Video process with success! Now, let's do the next one!"
+    print "Video process with success! Now, let"s do the next one!"
 
-print 'Done!'
+print "Done!"
